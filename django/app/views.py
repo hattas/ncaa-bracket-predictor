ace1effb2b49204729d9adabd9bc5a78c333186c
John Hattas <johnhattas@gmail.com>
2018-04-03 22:47:20 -0500

    reformat kevins indicators readme and rename some folders

---
+++ django/app/views.py
@@ -0,0 +1,52 @@
+"""
+Definition of views.
+"""
+
+from django.shortcuts import render
+from app import generate_bracket
+from win32timezone import now
+from django.http import HttpRequest
+from django.template import RequestContext
+from datetime import datetime
+listResults = []
+listIndicator = []
+listOrder = []
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'html/index.html',
+        {
+        }
+    )
+
+def bracket(request):
+    inp_value = request.GET.get('results', 'This is a default value')
+    context = {'inp_value': inp_value}
+    listResults = generate_bracket.get_tourney_results(2015, [inp_value])
+    listOrder = generate_bracket.get_tourney_order(2015)
+    return render(
+        request,
+        'html/bracket.html',
+        {
+            'round1':listOrder,
+            'roundOthers':listResults
+
+        }
+    )
+
+
+# def bracket(request):
+#    listResults = generateBracket.get_tourney_results(2015, ['OPF'])
+#    listOrder = generateBracket.get_tourney_order(2015)
+#    """Renders the home page."""
+#    assert isinstance(request, HttpRequest)
+#    return render(
+#        request,
+#        'html/bracket.html',
+#        {
+#            'round1':listOrder,
+#            'roundOthers':listResults
+#
+#        }
