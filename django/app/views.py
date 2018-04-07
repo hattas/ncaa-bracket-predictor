41d6b196842b28a7d036929ee694e76c7c3b514d
sba29 <berkhoutalex@gmail.com>
2018-04-07 15:13:25 -0500

    Added year select and fixed double team in final

--- django/app/views.py
+++ django/app/views.py
@@ -21,17 +21,27 @@ def home(request):
         }
     )
 
+
+
 def bracket(request):
     inp_value = request.GET.get('results', 'This is a default value')
     context = {'inp_value': inp_value}
-    listResults = generate_bracket.get_tourney_results(2015, [inp_value])
-    listOrder = generate_bracket.get_tourney_order(2015)
+    year_Val = request.GET.get('yearSelect','This is a default value')
+    context = {'year_Val': year_Val}
+    year = int(year_Val)
+    listResults = generate_bracket.get_tourney_results(year, [inp_value])
+    listOrder = generate_bracket.get_tourney_order(year)
+    if listResults[5][0] == listResults[4][1]:
+        loser = listResults[4][0]
+    else:
+        loser = listResults[4][1]
     return render(
         request,
         'html/bracket.html',
         {
             'round1':listOrder,
-            'roundOthers':listResults
+            'roundOthers':listResults,
+            'loser':loser
 
         }
     )
